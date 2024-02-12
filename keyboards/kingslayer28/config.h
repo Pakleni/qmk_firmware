d9dfeb85ed9ccb503013ed9be5958631126b72be
pakleni <ognjenbjel@protonmail.com>
2024-02-12 10:56:36 +0100

    add 2 layers, move rules

--- keyboards/kingslayer28/config.h
+++ keyboards/kingslayer28/config.h
@@ -3,21 +3,6 @@
 
 #pragma once
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
 #define RGB_MATRIX_LED_COUNT 28
 #define DRIVER_COUNT 1
 #define DRIVER_ADDR_1 0b1110100

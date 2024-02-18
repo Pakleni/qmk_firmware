4260821697ffa7c82e76cb344f9788bd490bde28
Ognjen Bjeletic <ognjenbjel@protonmail.com>
2024-02-18 11:01:28 +0100

    keyboard software v1

--- keyboards/kingslayer28/halconf.h
+++ keyboards/kingslayer28/halconf.h
@@ -3,8 +3,5 @@
 #ifdef RGB_MATRIX_ENABLE
 #define HAL_USE_I2C TRUE
 #endif
-#ifdef LED_MATRIX_ENABLE
-#define HAL_USE_I2C TRUE
-#endif
 
 #include_next <halconf.h>
--- keyboards/kingslayer28/keymaps/via/keymap.c
+++ keyboards/kingslayer28/keymaps/via/keymap.c
@@ -34,3 +34,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
+
+// THIS IS TESTING CODE
+#ifdef RGB_MATRIX_ENABLE
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    for (uint8_t i = led_min; i < led_max; i++) {
+        switch(get_highest_layer(layer_state|default_layer_state)) {
+            case 0:
+                rgb_matrix_set_color(i, RGB_RED);
+                break;
+            case 1:
+                rgb_matrix_set_color(i, RGB_GREEN);
+                break;
+            case 2:
+                rgb_matrix_set_color(i, RGB_BLUE);
+                break;
+            default:
+                break;
+        }
+    }
+    return false;
+}
+#endif
